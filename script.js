d07de41cd1aa5676a64ea570af8955a4b03caed4
manu <somamanojshm.22@gmail.com>
2025-05-14 13:17:53 +0530

    "Quiz-app completed"

---
+++ script.js
@@ -0,0 +1,145 @@
+const questions = [
+    {
+        question: "India is great country",
+        answers: [
+            { text: "True", correct: true },
+            { text: "False", correct: false }
+        ]
+    },
+    {
+        question: "India is great country",
+        answers: [
+            { text: "True", correct: true },
+            { text: "False", correct: false }
+        ]
+    },
+    {
+        question: "India is great country",
+        answers: [
+            { text: "True", correct: true },
+            { text: "False", correct: false }
+        ]
+    },
+    {
+        question: "India is great country",
+        answers: [
+            { text: "True", correct: true },
+            { text: "False", correct: false }
+        ]
+    },
+    {
+        question: "India is great country",
+        answers: [
+            { text: "True", correct: true },
+            { text: "False", correct: false }
+        ]
+    },
+    {
+        question: "India is great country",
+        answers: [
+            { text: "True", correct: true },
+            { text: "False", correct: false }
+        ]
+    },
+    {
+        question: "India is great country",
+        answers: [
+            { text: "True", correct: true },
+            { text: "False", correct: false }
+        ]
+    }
+];
+
+const questionElement = document.getElementById("question");
+const answerButtons = document.getElementById("answer-buttons");
+const nextButton = document.getElementById("next-btn");
+
+let currentQuestionIndex = 0;
+let score = 0;
+
+function startQuiz() {
+    currentQuestionIndex = 0;
+    score = 0;
+    nextButton.innerHTML = "next";
+    showQuestion();
+}
+
+function resetState() {
+    
+    nextButton.style.display="none";
+    while(answerButtons.firstChild){
+        answerButtons.removeChild(answerButtons.firstChild);
+    }
+}
+
+function showQuestion() {
+    resetState();
+    let currentQuestion = questions[currentQuestionIndex];
+    let questionNo = currentQuestionIndex + 1;
+    questionElement.innerHTML = questionNo + ". " + currentQuestion.question;
+
+    currentQuestion.answers.forEach(answer => {
+        const button = document.createElement("button");
+        button.innerHTML = answer.text;
+        button.classList.add("bttn");
+        answerButtons.appendChild(button);
+        if(answer.correct){
+            button.dataset.correct=answer.correct;
+        }
+        // Adding click functionality. 
+        button.addEventListener("click",selectAnswer);
+    });
+}
+function selectAnswer(e) {
+    const selectBtn=e.target;
+    const isCorrect=selectBtn.dataset.correct ==="true";
+    if(isCorrect){
+        selectBtn.classList.add("correct");
+        score++;
+    }else{
+        selectBtn.classList.add("incorrect");
+    }
+    // To add green color for the correct answer after selected once.
+    Array.from(answerButtons.children).forEach(button =>{
+
+        if(button.dataset.correct ==="true"){
+            // add green color once a wrong option selected.
+            button.classList.add("correct");
+        }
+        button.disabled = true;
+        // button.classList.add("correct");
+    });
+    nextButton.style.display= "block";
+}
+
+function showScore(){
+    resetState();
+    // to display score 
+    questionElement.innerHTML = `You scored ${score} out of ${questions.length}!`;
+    nextButton.innerHTML ="Play Again";
+    nextButton.style.display= "block"
+}
+
+// function to handle next button
+function handleNextButton(){
+    currentQuestionIndex++;
+    if(currentQuestionIndex < questions.length){
+        showQuestion();
+    }
+    else{
+        showScore();
+    }
+}
+
+
+// function for next button
+
+nextButton.addEventListener("click",()=>{
+    if(currentQuestionIndex < questions.length){
+        handleNextButton();
+    }
+    else{
+        startQuiz();
+    }
+})
+startQuiz();
